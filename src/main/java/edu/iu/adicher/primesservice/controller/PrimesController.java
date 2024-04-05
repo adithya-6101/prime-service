68c4a8aa68d3f114aaf89b34a1c9880fb66cf505
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-04 21:35:59 -0400

    Update PrimesController.java

--- src/main/java/edu/iu/adicher/primesservice/controller/PrimesController.java
+++ src/main/java/edu/iu/adicher/primesservice/controller/PrimesController.java
@@ -1,5 +1,6 @@
 package edu.iu.adicher.primesservice.controller;
 
+import edu.iu.adicher.primesservice.rabbitmq.MQSender;
 import edu.iu.adicher.primesservice.service.IPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
+    private final MQSender mqSender;
 
-
-    public PrimesController(IPrimesService primesService)
+    public PrimesController(IPrimesService primesService, MQSender mqSender)
     {
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
 
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
+        boolean result = primesService.isPrime(n);
+        mqSender.sendMessage(n,result);
+        return result;
     }
 }
