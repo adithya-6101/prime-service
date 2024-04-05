47e27c778896ef0769ff7040b59056b4a9cc141c
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-04 21:40:47 -0400

    Update IAuthenticationService.java

--- src/main/java/edu/iu/adicher/primesservice/service/IAuthenticationService.java
+++ src/main/java/edu/iu/adicher/primesservice/service/IAuthenticationService.java
@@ -5,7 +5,7 @@ import edu.iu.adicher.primesservice.model.Customer;
 import java.io.IOException;
 
 public interface IAuthenticationService {
-    boolean register (Customer customer) throws IOException;
+    Customer register (Customer customer) throws IOException;
 
 
     boolean login(String username, String password) throws IOException;
