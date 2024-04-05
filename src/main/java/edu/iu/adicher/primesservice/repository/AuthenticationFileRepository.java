76932db39377cedd21136a205676d73585fa642c
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-04 21:38:02 -0400

    Update AuthenticationFileRepository.java

--- src/main/java/edu/iu/adicher/primesservice/repository/AuthenticationFileRepository.java
+++ src/main/java/edu/iu/adicher/primesservice/repository/AuthenticationFileRepository.java
@@ -1,7 +1,6 @@
 package edu.iu.adicher.primesservice.repository;
 
 import edu.iu.adicher.primesservice.model.Customer;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
