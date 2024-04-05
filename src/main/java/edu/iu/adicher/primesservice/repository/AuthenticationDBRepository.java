85fc194747bde62e2ea3f35c578cfdf0a9315501
ADITHYA CHERUKURI <92886634+adithya-6101@users.noreply.github.com>
2024-04-04 21:37:33 -0400

    Add files via upload

---
+++ src/main/java/edu/iu/adicher/primesservice/repository/AuthenticationDBRepository.java
@@ -0,0 +1,10 @@
+package edu.iu.adicher.primesservice.repository;
+
+import edu.iu.adicher.primesservice.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthenticationDBRepository extends CrudRepository<Customer, String> {
+    Customer findByUsername(String username);
+}
